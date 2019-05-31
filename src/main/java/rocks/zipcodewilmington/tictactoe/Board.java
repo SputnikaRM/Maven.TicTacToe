38d18c99270f5f7c10bcf767306c47891ecae3d4
sputnika <sputnika91@gmail.com>
2019-05-31 15:48:17 -0400

    update-ish

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,10 +4,13 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
-    }
+    public Board(Character[][] matrix) {}
 
     public Boolean isInFavorOfX() {
+        for (int x=0; x<3; x++){
+            if
+
+        }
         return null;
     }
 
@@ -23,4 +26,20 @@ public class Board {
         return null;
     }
 
+    public Boolean rows (char a0,char a1, char a2) {
+        for (int x =0; x<3; x++){
+            if((a0 == a1) && (a1 == a2))
+                return true;
+        }
+        return false;
+    }
+
+    public Boolean columns (char b0, char b1, char b2){
+        for (int y=0; y<3; y++){
+            if((b0 == b1) && (b1 == b2))
+                return true;
+        } return false;
+    }
+
+    
 }
